5699ac2e386480421decc15e71b12b3ba6a490bf
Jessa Challa <jessa.challa@gmail.com>
2023-01-10 13:48:31 -0500

    updates the game to make it interactive

--- main.js
+++ main.js
@@ -1,70 +1,118 @@
-//When the page is loaded, the USER is asked if they want to play the game
-let startGame = prompt("Do you want to play the game? Please type 'Yes' or 'No'.")
+//Prompt the USER to start the game
+function startGame() {
+    let start = confirm("Do you want to play the game?");
+    
+    //If yes, go to "Name Your Warrior" prompt
+    if (start === true) {
+        let user = prompt("Name Your Warrior:");
+        //If user clicks 'cancel'
+        if (user === null) {
+            document.write(`<button type="button" onClick="window.location.reload()">
+            Start Game
+            </button>`)
+        }
+        //If user doesn't type a name
+        else if (!user.length) {
+            document.write(`<p>You didn't name your warrior!</p>
+            <button type="button" onClick="window.location.reload()">
+            Try again
+            </button>`)
+        }
+        //When user names warrior, the startCombat function is called
+        else {
+            startCombat(user);
+        }
 
-//If yes, the game starts
-if (startGame === 'Yes') {
-    let userHealth = 40;
+    }
+    //If no
+    else if (start === false) {
+        alert("Come back when you're up for the CHALLENGE!");
+        document.write(`<button type="button" onClick="window.location.reload()">
+            Start Game
+            </button>`)
+    }
+}
+
+//A function for resetting the stats
+function resetStats() {
+    userHealth = 15;
+    grantHealth = 10;
+    leaveGame = true;
+    wins = 0;
+}
+
+
+//The beginning of the startCombat function
+function startCombat(user) {
+    
+    //stats are set
+    let userHealth = 15;
     let grantHealth = 10;
+    let leaveGame = true;
     let wins = 0;
-    let user = prompt("Name Your Warrior:");
 
     //This is the while loop for the game
-    while (userHealth > 0 && grantHealth > 0 && wins < 3) {
-        //This is how much health Grant will lose
-        let attackGrant = (Math.floor(Math.random() * (2) + 1));
-        //This is how much health the USER will lose. 
-        //Justin, I changed it to 2.75 becuase it was too hard for the USER to lose when the USER and Grant had the 1 or 2 health damage. Now the USER will take a 1, 2, or 3 damage.
-        let attackUser = (Math.floor(Math.random() * (2.75) + 1));
-        userHealth = userHealth - attackUser;
-        grantHealth = grantHealth - attackGrant;
-        document.write(`<p>${user} has ${userHealth} health left.</p>`);
-        document.write(`<p>The Evil Grant has ${grantHealth} health left.</p>`);
+    while (userHealth > 0 && grantHealth > 0 && wins < 3 && leaveGame === true) {
+        userHealth = userHealth - getDamage();
+        grantHealth = grantHealth - getDamage();
+        alert(`${user} has ${userHealth} health left.
+        The Evil Grant has ${grantHealth} health left.`);
 
         //If Grant and User both lose
         if (userHealth <= 0 && grantHealth <= 0) {
-            document.write("<p>TIE! You must defeat the Evil Grant!</p>");
-            document.write(`<button type="button" onClick="window.location.reload()">
-        Play Again
-    </button>`)
+            alert("TIE! You must defeat the Evil Grant! Play Again!");
+            resetStats()
+            startGame()
         }
 
         //If User loses
         else if (userHealth <= 0) {
-            document.write("<p>YOU WERE DEFEATED by EVIL GRANT!</p>");
-            document.write(`<button type="button" onClick="window.location.reload()">
-        Play Again?
-    </button>`)
+            alert("YOU WERE DEFEATED by EVIL GRANT! Play Again!");
+            resetStats()
+            startGame()
         }
 
         //If User Wins
         else if (grantHealth <= 0) {
             wins = wins + 1;
+            userHealth = 15;
             grantHealth = 10;
-            document.write("<p>You WON this round</p>");
-            document.write(`<p>You have won ${wins} time(s)!</p>`);
+            alert("You WON this round");
+            alert(`You have won ${wins} time(s)!`);
+            if (wins < 3) {
+                leaveGame = confirm("You need to win 3 rounds. Do you want to continue fighting?");
+
+                if (leaveGame === false) {
+                    alert("You have surrendered!")
+                    document.write(`<button type="button" onClick="window.location.reload()">
+            Play Again?
+            </button>`)
+                }
+            }
+        }
+
+        //This displays when Grant is defeated 3 times AND there is not a losing tie between USER and Grant
+        if (wins == 3 && userHealth > 0 && leaveGame === true) {
+            let restart = confirm("You have DEFEATED the EVIL GRANT 3 times and SAVED the UNIVERSE! Play again?");
+            if (restart === true) {
+                let user = prompt("Name Your Warrior:");
+                startCombat(user);
+            }
+            else if (restart === false) {
+                alert("Come back when you're up for the CHALLENGE!");
+                document.write(`<button type="button" onClick="window.location.reload()">
+            Start Game
+            </button>`)
+            }
         }
     }
-
-    //This displays when Grant is defeated 3 times AND there is not a losing tie between USER and Grant
-    if (win = 3 && userHealth > 0) {
-        document.write("<p>You have DEFEATED the EVIL GRANT 3 times and SAVED the UNIVERSE!</p>")
-        document.write(`<button type="button" onClick="window.location.reload()">
-        Play Again?
-    </button>`)
-    }
-}
-//If the USER does not want to play
-else if (startGame === 'No') {
-    document.write("<p>You don't want to play? Come back when you do!</p>")
-    document.write(`<button type="button" onClick="window.location.reload()">
-        Play the Game
-    </button>`)
 }
 
-//If the USER doesn't type 'Yes' or 'No' correctly
-else {
-    document.write("<p>You have not used a proper command. Please try again.</p>")
-    document.write(`<button type="button" onClick="window.location.reload()">
-        Try Again
-    </button>`)
+
+function getDamage() {
+    let damage = Math.floor(Math.random() * (5) + 1);
+    return (damage);
 }
+
+//The startGame function is called to begin the game
+startGame();
